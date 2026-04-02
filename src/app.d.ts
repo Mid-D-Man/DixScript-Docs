8efe806cf06b36c96c66b4e22dbdb55ac68ffb4d
Abdulhamid Manman Suleiman <94022993+Mid-D-Man@users.noreply.github.com>
2026-04-02 21:43:41 +0100

    Update app.d.ts

--- src/app.d.ts
+++ src/app.d.ts
@@ -4,6 +4,8 @@ declare global {
       env: {
         MDIX_REGISTRY: R2Bucket;
         ADMIN_KEY: string;
+        SUPABASE_URL: string;
+        SUPABASE_SERVICE_ROLE_KEY: string;
       };
     }
   }
